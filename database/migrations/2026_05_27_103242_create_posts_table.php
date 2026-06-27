3d61bff24a8e73cb96f6d47c65674288373697da
Graham Blackledge <140705543+GrahamBlackledge@users.noreply.github.com>
2026-06-27 13:17:46 +1200

    Updated post/user migrations

--- database/migrations/2026_05_27_103242_create_posts_table.php
+++ database/migrations/2026_05_27_103242_create_posts_table.php
@@ -12,12 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps();
-        });
+        $table->id();
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->string('title');
+        $table->text('body');
+        $table->string('filter')->nullable();
+        $table->string('image')->nullable();
+        $table->timestamps();
+});
     }
 
     /**

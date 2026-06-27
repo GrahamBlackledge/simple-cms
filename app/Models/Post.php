245cf1eb845786ca00db0c66b860dafde94473e1
Graham Blackledge <140705543+GrahamBlackledge@users.noreply.github.com>
2026-06-27 13:29:34 +1200

    Updated models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,16 +2,26 @@
 
 namespace App\Models;
 
+use Database\Factories\PostFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
+    /** @use HasFactory<PostFactory> */
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'title',
         'body',
+        'filter',
+        'image',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
